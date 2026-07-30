140df49c24ec630499a51bba1f4708e13ac4e3d0
kingdomlightnetwork <pasterparvez888@gmail.com>
2026-07-30 19:27:35 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Kingdom Light Network
 // Powered by Christ Church
 
@@ -6,12 +5,41 @@ document.addEventListener("DOMContentLoaded", function () {
 
     console.log("Kingdom Light Network is running");
 
+
+    const menuButton = document.querySelector(".menu-toggle");
+    const nav = document.querySelector("nav");
+
+
+    if (menuButton) {
+
+        menuButton.addEventListener("click", function () {
+
+            if (nav.style.display === "block") {
+
+                nav.style.display = "none";
+
+            } else {
+
+                nav.style.display = "block";
+
+            }
+
+        });
+
+    }
+
+
     const buttons = document.querySelectorAll("button");
 
     buttons.forEach(function(button) {
+
         button.addEventListener("click", function() {
+
             console.log("Button clicked:", button.innerText);
+
         });
+
     });
+
 
 });

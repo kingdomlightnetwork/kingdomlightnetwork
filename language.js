8fc0b22b0eab2c04b316dc051954f72ac124acfb
kingdomlightnetwork <pasterparvez888@gmail.com>
2026-08-04 17:58:26 +0500

    Update language.js

--- language.js
+++ language.js
@@ -1,1 +1,19 @@
+function changeLanguage(lang) {
 
+    localStorage.setItem("language", lang);
+
+    alert("Language changed to: " + lang);
+
+}
+
+window.onload = function () {
+
+    let savedLanguage = localStorage.getItem("language");
+
+    if (!savedLanguage) {
+
+        localStorage.setItem("language", "en");
+
+    }
+
+};

a721970bb4c9950fc1cee338065a87d448acb2e3
kingdomlightnetwork <pasterparvez888@gmail.com>
2026-08-04 17:55:41 +0500

    Update translations.js

--- translations.js
+++ translations.js
@@ -1,1 +1,77 @@
 
+const translations = {
+
+    en: {
+
+        home: "Home",
+        live: "Live",
+        sermons: "Sermons",
+        bible: "Bible",
+        ai: "AI",
+        welfare: "Welfare",
+        prayer: "Prayer",
+        media: "Media",
+        contact: "Contact",
+        register: "Register",
+
+        language: "Language"
+
+    },
+
+
+
+    ur: {
+
+        home: "ہوم",
+
+        live: "لائیو",
+
+        sermons: "پیغامات",
+
+        bible: "بائبل",
+
+        ai: "اے آئی",
+
+        welfare: "فلاحی خدمت",
+
+        prayer: "دعائیہ مرکز",
+
+        media: "میڈیا",
+
+        contact: "رابطہ",
+
+        register: "رجسٹریشن",
+
+        language: "زبان"
+
+    },
+
+
+
+    hi: {
+
+        home: "होम",
+
+        live: "लाइव",
+
+        sermons: "संदेश",
+
+        bible: "बाइबल",
+
+        ai: "एआई",
+
+        welfare: "वेलफेयर",
+
+        prayer: "प्रार्थना",
+
+        media: "मीडिया",
+
+        contact: "संपर्क",
+
+        register: "रजिस्ट्रेशन",
+
+        language: "भाषा"
+
+    }
+
+};

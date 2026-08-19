75ac14cc41c51c15bbf9c44b731c138e59068091
kingdomlightnetwork <pasterparvez888@gmail.com>
2026-08-19 18:00:40 +0500

    Update bible-data.js

--- bible-data.js
+++ bible-data.js
@@ -1,1 +1,25 @@
+/* =====================================================
+   KINGDOM LIGHT NETWORK
+   LOCAL BIBLE DATA
+   KING JAMES VERSION
+===================================================== */
 
+const KJV_BIBLE = {
+
+    new: {
+
+        Mark: {
+
+            3: {
+
+                6: "And the Pharisees went forth, and straightway took counsel with the Herodians against him, how they might destroy him.",
+
+                16: "And Simon he surnamed Peter."
+
+            }
+
+        }
+
+    }
+
+};
